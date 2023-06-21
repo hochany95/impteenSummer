b8d1e09550dda7cebb2b5ab4bac6edeebce433e7
hochan.yun <hochany95@gmail.com>
2023-06-21 22:37:50 +0900

    first fragment and init activity

---
+++ EscapeRoomApp/app/src/main/java/com/impacteen/hochan/escaperoomapp/InitFragment.java
@@ -0,0 +1,36 @@
+package com.impacteen.hochan.escaperoomapp;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.impacteen.hochan.escaperoomapp.databinding.FragmentInitBinding;
+
+public class InitFragment extends Fragment {
+
+    FragmentInitBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+         binding = FragmentInitBinding.inflate(inflater, container, false);
+         binding.button4.setOnClickListener(view -> {
+             Toast.makeText(container.getContext(), "next script", Toast.LENGTH_SHORT).show();
+         });
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
+
+}
--- EscapeRoomApp/app/src/main/java/com/impacteen/hochan/escaperoomapp/MainActivity.java
+++ EscapeRoomApp/app/src/main/java/com/impacteen/hochan/escaperoomapp/MainActivity.java
@@ -1,14 +1,27 @@
 package com.impacteen.hochan.escaperoomapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentResultListener;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.impacteen.hochan.escaperoomapp.databinding.FragmentInitBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+
+    InitFragment initFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initFragment = new InitFragment();
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction ft = manager.beginTransaction();
+        ft.add(R.id.fragmentContainer, initFragment);
+        ft.commit();
     }
 }
