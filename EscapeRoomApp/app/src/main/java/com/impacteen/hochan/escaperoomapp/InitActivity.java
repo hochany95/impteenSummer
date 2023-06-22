70f29ad09d6c63e1b0f394036ac1a19bf1a45cdb
hochan.yun <hochany95@gmail.com>
2023-06-22 22:10:59 +0900

    init proto doen

--- EscapeRoomApp/app/src/main/java/com/impacteen/hochan/escaperoomapp/InitActivity.java
+++ EscapeRoomApp/app/src/main/java/com/impacteen/hochan/escaperoomapp/InitActivity.java
@@ -1,31 +1,87 @@
 package com.impacteen.hochan.escaperoomapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DigitalClock;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextClock;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class InitActivity extends AppCompatActivity {
 
-    TextView textView;
+    TextView backgroundView;
+    TextClock textClock;
     Button initBtn;
+    AlertDialog initDialog;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_init);
 
-        textView = (TextView) findViewById(R.id.initTextView);
+        backgroundView = (TextView) findViewById(R.id.initTextView);
+        textClock = (TextClock) findViewById(R.id.textClock);
+        textClock.setAlpha((float) 0.4);
         initBtn = (Button) findViewById(R.id.initNextBtn);
+        createPasswordDialog();
         initBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "타이핑 끝", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "어따쓰지", Toast.LENGTH_SHORT).show();
+            }
+        });
+        textClock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showPasswordDialog();
+
             }
         });
     }
+
+    private void createPasswordDialog() {
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        LinearLayout dialogLayout = (LinearLayout) inflater.inflate(R.layout.init_password_dialog, null);
+
+        EditText inputEditText = (EditText) dialogLayout.findViewById(R.id.editTextText);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(dialogLayout);
+        builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                String inputString = inputEditText.getText().toString();
+                inputEditText.setText("");
+                if(inputString.equalsIgnoreCase("three")){
+
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                }else{
+                    inputEditText.setHint("input here..");
+                    Toast.makeText(getApplicationContext(), "Wrong, try again", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+        initDialog = builder.create();
+    }
+
+    private void showPasswordDialog(){
+        initDialog.show();
+    }
+
+
+
 }
