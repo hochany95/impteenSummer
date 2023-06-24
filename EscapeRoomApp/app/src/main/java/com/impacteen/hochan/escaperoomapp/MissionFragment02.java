998b607eda781458d85c93f59672f78e8b1ded59
hochan.yun <hochany95@gmail.com>
2023-06-24 09:43:23 +0900

    windowfullscreen and add fragments

---
+++ EscapeRoomApp/app/src/main/java/com/impacteen/hochan/escaperoomapp/MissionFragment02.java
@@ -0,0 +1,14 @@
+package com.impacteen.hochan.escaperoomapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MissionFragment02 extends Fragment {
+
+
+}
